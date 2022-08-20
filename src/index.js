9ca4ad4c24f3dc86ff3a034342c0c8965aaf3ab5
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-08-20 13:30:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,4 +15,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import '';
