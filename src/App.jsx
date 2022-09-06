297750c9590fd09f78e2b1360904f07bf740ec90
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-06 11:51:58 +0100

    !

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import Profile from 'components/Profile/Profile';
+import user from 'components/Profile/user.json';
+
+
 export const App = () => {
   return (
     <div
@@ -8,9 +12,16 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      React homework template
+      }}>
+      <Profile
+      username = {user.username}
+      tag = {user.tag}
+      location = {user.location}
+      avatar = {user.avatar}
+      stats = {user.stats}
+      />
     </div>
   );
 };
+
+export default App
