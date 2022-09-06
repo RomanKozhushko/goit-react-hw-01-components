437421ab5795d9f730d21fe3b3d81c93961fcd2a
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-06 10:41:27 +0100

    Update componentProfile.jsx

--- src/components/Profile/componentProfile.jsx
+++ src/components/Profile/componentProfile.jsx
@@ -1,3 +1,17 @@
+import PropTypes from 'prop-types';
+
+ Profile.propTypes = {
+    username : PropTypes.string,
+    tag : PropTypes.string,
+    location : PropTypes.string,
+    avatar : PropTypes.string,
+    stats : PropTypes.object,
+  }
+
+
+
+
+
 <div class="profile">
   <div class="description">
     <img
@@ -27,40 +41,3 @@
 </div>
 
 
-// Компонент повинен приймати кілька пропсів з інформацією про користувача:
-
-// username — ім'я користувача
-// tag — тег в соціальній мережі без @
-// location — місто і країна
-// avatar — посилання на зображення
-// stats — об'єкт з інформацією про активності
-
-import user from 'user'
-
-<Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
-
- Profile.propTypes = {
-    username : PropTypes.string,
-    tag : PropTypes.string,
-    location : PropTypes.string,
-    avatar : PropTypes.string,
-    stats : PropTypes.object,
-  
-}
-  {
-  "username": "Jacques Gluke",
-  "tag": "jgluke",
-  "location": "Ocho Rios, Jamaica",
-  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  "stats": {
-    "followers": 5603,
-    "views": 4827,
-    "likes": 1308
-  }
-}
