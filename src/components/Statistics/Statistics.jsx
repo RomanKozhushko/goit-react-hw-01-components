5f1309411958dcb053d1ac62fcd1d26fb5168077
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-07 07:57:21 +0100

    k

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,9 +4,9 @@ import css from './Statistics.module.css'
 const Statistics = ({ title, stats }) => {
   return (
       <section className={css.statistics}>
-           { title ? <h2 class = "title">{title}</h2> : ""}
+          {title ? <h2 className={css.title}>{title}</h2> : ""}
 
-          <ul className={css.stat - list}>
+          <ul className={css.statlist}>
        {stats.map (stat => {
         return (
             <li className={css.item} key={stat.id}>
