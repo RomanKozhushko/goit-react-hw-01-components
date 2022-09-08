14394acfd11ed600b06ebd36c912a2c40572e341
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-08 09:38:59 +0100

    restart

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import Profile from 'components/Profile/Profile';
+import user from 'components/Profile/user.json';
+
+import data from "components/Statistics/data.json"
+import Statistics from 'components/Statistics/Statistics';
+
+import friends from "components/FriendList/friends.json";
+import FriendList from "components/FriendList/FriendList";
+
+import transactions from "components/TransactionHistory/transactions.json";
+import TransactionHistory from "components/TransactionHistory/TransactionsHistory";
+
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+      username = {user.username}
+      tag = {user.tag}
+      location = {user.location}
+      avatar = {user.avatar}
+      stats = {user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+
+  )
+}
+
+export default App
